e46878f17bd3d1e30d871106ea967a3eced57c68
Kritik Modi <61862301+kritikmodi@users.noreply.github.com>
2023-08-14 22:41:23 +0530

    add s3 support

--- app_files/datatrader_services/app.py
+++ app_files/datatrader_services/app.py
@@ -3,6 +3,13 @@ import yfinance as finance
 from prophet.serialize import model_from_json
 from os.path import abspath, dirname, join
 
+import pandas as pd
+import dt.user
+from dt.cloud.blob_storage import BlobStorage
+import io
+
+import json
+
 app = Flask(__name__)
 
 @app.route('/data', methods=['POST'])
@@ -20,16 +27,26 @@ def fetch_data():
 @app.route('/forecast', methods=['POST'])
 def forecast_data():
 
+    dt.user.set_current_user(dt.user.signed_user())
+
+    s3 = BlobStorage()
+
     company = request.form['text']
-    with open(join(dirname(abspath(__file__)), f'{company}.json'), 'r') as fin:
-        m = model_from_json(fin.read())  # Load model 
+
+    file_name = company+".json"
+
+    encoded_file = io.BytesIO(s3.get_object(file_name))
+   
+    m = model_from_json(encoded_file.read().decode())  # Load model 
+
     future = m.make_future_dataframe(periods=200)
+
     forecast = m.predict(future)
     
     return jsonify(data = forecast.to_json(orient='split', date_format='iso'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
     
 def __service_main__(port):
 	app.run('0.0.0.0', port=int(port), debug=False)
